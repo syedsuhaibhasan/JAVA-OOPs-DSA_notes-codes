9fcef4e0e9d8c29aaffda35e85a5e5ff1c92e0f2
Syed Muhammad Suhaib Hasan <syedsuhaibhasan888@gmail.com>
2025-11-30 18:50:10 +0500

    Revert "commit"
    This reverts commit 85e4755d60622f0cbfc9825ce6fb6aa3c090418c.

--- src/main/java/DSA/Queue/CircularQueue.java
+++ src/main/java/DSA/Queue/CircularQueue.java
@@ -3,68 +3,12 @@ public class CircularQueue {
     int[] queue;
     int sizeOfqueue;
     int max;
-    int front,rear;
+    int front,rear=-1;
     
     public CircularQueue(int sizeOfqueue){
     this.queue=new int[sizeOfqueue];
     this.max=sizeOfqueue;
-    this.front=-1;
-    this.rear=-1;
     }
     
-    public void enQueue(int item){
-        if ((rear+1)%max==front) {
-            System.out.println("OVERFLOW");
-            return;
-        }if (front==-1&&rear==-1) {
-            front=rear=0;
-        }else{
-            rear=(rear+1)%max;
-        }
-        queue[rear]=item;
-    }
-    
-    public void deQueue(){
-        if (front==-1) {
-            System.out.println("UNDERFLOW");
-            return;
-        }if (front==rear) {
-            front=rear=-1;
-        }else{
-            front=front+1;
-        }
-    }
-    
-    public void print(){
-        if (front == -1) {
-            System.out.println("Queue is empty");
-            return;
-        }
-
-        System.out.print("Queue: ");
-
-        int i = front;
-        while (true) {
-            System.out.print(queue[i] + " ");
-
-            if (i == rear) break;
-
-            i = (i + 1) % max;
-        }
-        System.out.println();
-    }
-    
-    public static void main(String[] args) {
-        CircularQueue obj = new CircularQueue(3);
-        obj.enQueue(1);
-        obj.enQueue(2);
-        obj.enQueue(3);
-        obj.print();
-        obj.deQueue();
-        obj.deQueue();
-        obj.print();
-        obj.enQueue(50);
-        obj.print();
-    }
-    
+    public void enQueue(){}
 }
